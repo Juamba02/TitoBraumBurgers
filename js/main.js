55b82b03d3abeeb25abc93031df6603e48fc6a3c
Juamba02 <juanaramberri02@gmail.com>
2022-11-05 20:49:03 -0300

    Agregadas las funcionalidades del carrito

--- js/main.js
+++ js/main.js
@@ -1,21 +1,88 @@
+// Declaro el array del carrito y declaro como variables los objetos del DOM que voy a usar
+
+const carrito = [];
+
 const pedido = document.querySelector("#btn-pedido");
 const carritoContainer = document.querySelector("#carrito-container");
 const salir = document.querySelector("#salir");
 const equis = document.querySelector("#x");
 const divTarjetas = document.querySelector("#divTarjetas");
+const carritoProductos = document.querySelector("#productoEnCarrito");
+const numCarrito = document.querySelector("#numCarrito");
+const precioTotal = document.querySelector("#precioTotal");
+
+// Funciones
+
+const renderPedido = () => {
+    carritoProductos.innerHTML = "";
+
+    carrito.forEach((hamburguesa) => {
+        const div = document.createElement("div");
+        div.className = "productoEnCarrito"
+        div.innerHTML =`
+        <img src="${hamburguesa.imgSrc}" alt="">
+            <p>${hamburguesa.nombre}</p>
+            <p>Precio: $${hamburguesa.precio}</p>
+            <p>x${hamburguesa.cantidad}</p>
+            <button class="eliminar">Eliminar</button>
+            `
+    carritoProductos.append(div);
+    });
+}
+
+const renderNumCarrito = () => {
+    numCarrito.innerText = carrito.length;
+}
+
+const renderTotal = () => {
+    let total = carrito.reduce((total, pedido) => total += pedido.precio, 0);
+    precioTotal.innerText = total;
+}
+
+const agregarAlCarrito = (id) => {
+    const producto = hamburguesas.find((item) => item.id === id);
+    carrito.push(producto);
+
+    renderCarrito();
+}
+
+const renderCarrito = () => {
+    renderPedido();
+    renderNumCarrito();
+    renderTotal();
+}
+
+// Creo las tarjetas en base a los productos que tengo en stock
 
 hamburguesas.forEach((hamburguesa) => {
-    divTarjetas.innerHTML += `
-        <div class="tarjetas">
+    if (hamburguesa.stock == 0){
+        return
+    }
+
+    const div = document.createElement("div");
+    div.className = "tarjetas";
+
+    div.innerHTML += `
             <img src="${hamburguesa.imgSrc}" alt="${hamburguesa.nombre}" class="imgTarjeta">
             <h5 class="tituloTarjeta">${hamburguesa.nombre}</h5>
             <p class="descripcion">${hamburguesa.desc}</p>
             <p class="precio">$${hamburguesa.precio}</p>
-            <button class="botonTarjeta">Agregar a mi pedido</button>
-        </div>
         `
+
+    const button = document.createElement("button");
+    button.className = "botonTarjeta";
+    button.innerHTML = "Agregar a mi pedido";
+
+    button.addEventListener("click", () => {
+        agregarAlCarrito(hamburguesa.id);
+    });
+
+    div.append(button);
+
+    divTarjetas.append(div);
 });
 
+// Creo el evento para abrir el modal del carrito
 
 pedido.addEventListener("click", (event) => {
     event.preventDefault();
@@ -29,4 +96,3 @@ salir.addEventListener("click", () => {
 equis.addEventListener("click", () => {
     carritoContainer.classList.remove("container-active");
 });
-
--- js/productos.js
+++ js/productos.js
@@ -3,48 +3,54 @@ const hamburguesas = [
         id: 0,
         nombre: "PoroBurger",
         precio: 1250,
-        cantidad: 0,
+        stock: 100,
         desc: "Doble Medallon de Carne, Aros de cebolla, Cheddar y Salsa BIG Braum.",
-        imgSrc: "../img/Big Onion.jpg"
+        imgSrc: "../img/Big Onion.jpg",
+        cantidad: 0
     },
     {
         id: 1,
         nombre: "Classic Bigote",
         precio: 1250,
         desc: "Doble Medallón de Carne, Cheddar, Pepinillos, Lechuga y Salsa TitoBraum.",
-        cantidad: 0,
-        imgSrc: "../img/Clasica.jpg"
+        stock: 100,
+        imgSrc: "../img/Clasica.jpg",
+        cantidad: 0
     },
     {
         id: 2,
         nombre: "Avocado Zac",
         precio: 1250,
         desc: "Doble Medallón de Carne, Cheddar, Crema de Palta, y Cebolla Encurtida.",
-        cantidad: 0,
-        imgSrc: "../img/Avocado.jpg"
+        stock: 100,
+        imgSrc: "../img/Avocado.jpg",
+        cantidad: 0
     },
     {
         id: 3,
         nombre: "Butter Blitz",
         precio: 1250,
         desc: "Doble Medallón de Carne, Manteca, Cebolla estofada y Cheddar.",
-        cantidad: 0,
-        imgSrc: "../img/ButterBurger.jpg"
+        stock: 100,
+        imgSrc: "../img/ButterBurger.jpg",
+        cantidad: 0
     },
     {
         id: 4,
         nombre: "Super Teemo",
         precio: 1350,
         desc: "Doble Medallón de Carne, Queso Tomme y Hongos de Pino Tratados.",
-        cantidad: 0,
-        imgSrc: "../img/SuperTeemo.jpg"
+        stock: 100,
+        imgSrc: "../img/SuperTeemo.jpg",
+        cantidad: 0
     },
     {
         id: 5,
         nombre: "Tito Braum",
         precio: 1490,
         desc: "Triple Medallón de Carne, Cheddar, Mozzarella, y Barbacoa TitoBraum.",
-        cantidad: 0,
-        imgSrc: "../img/TitoBraum.jpg"
+        stock: 100,
+        imgSrc: "../img/TitoBraum.jpg",
+        cantidad: 0
     }
 ];
